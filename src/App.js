870d83b9d026874b40dd520d898d364ea8dc10ac
Aisen Nogoibaev <aisennogoibaev999@gmail.com>
2021-12-11 14:12:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Routing from "./pages/Routes";
+
 
 function App() {
   return (
-    <div className="App">
-     test
-    </div>
+    <Routing/>
   );
 }
 
--- src/components/Spinners/FullSpinner.js
+++ src/components/Spinners/FullSpinner.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Spinner} from "react-bootstrap";
 
 const FullSpinner = () => {
     return (
-        <div>
-            Loading...
+        <div className="container">
+            <div className='d-flex justify-content-center m-auto'>
+                <Spinner animation="border" role="status" className=''>
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </div>
+
         </div>
     );
 };
